fca2d451f35f22c5efaaac4ac8c5aeee6de5bb2d
Bill Martin <bmartin@salientcdi.com>
2016-12-21 20:18:11 -0600

    Config ui jb2020 (#487)
    * add voice remote for mirror
    
    add connected/disconnected events
    
    * remote devtools kiosk reload
    
    * expose mirror remote IP and port
    
    * expose mirror remote IP and port
    
    * remote connection state management
    
    * add visual breakpoint for JS in index.html
    
    * Adding QR functionality using googleapis charts
    
    this will need to be revised as google can end use of this as it is depreciated...  This is more to provide a way of testing the concept and not delay development of the config pages...
    
    * Committing remaining changes for QRservice
    
    * Config UI Support added
    
    Add Config UI, must correct issue with accessing config variable
    from all areas... app.js throws reference error regarding config
    
    Added pulling project ID from keyfile for sonus. removed projectId
    from config.
    
    TODO:
      -- need to refine formatting for config UI.
      -- add form array to schema
      -- add conditions to form
    
    * added ability to import config.js file
    on run if it exists. fine tuned overall performance.
    
    todo: add push to config variable in DOM scope
    
    * Correct issues with configUI
    added functionality to import config.js and rename to config.old.js

--- config-index.js
+++ config-index.js
@@ -1,25 +1,65 @@
+  const fs = require('fs')
   var config = ""
   var configMaster = "" 
-  var configJSON = ""
-  var configFN = __dirname + "/config.json"
+  var configSchema = ""
+  var configJson
+  var configJsonFN = __dirname + "/config.json"
+  var configJsFN = __dirname + "/config.js"
+  var configOldJsFN = __dirname + "/config.old.js"
   var configMasterFN = __dirname + "/remote/config.master.json"
-  var configJsonFN = __dirname + "/remote/config.schema.json"
+  var configSchemaFN = __dirname + "/remote/config.schema.json"
 
   function getFiles(){
-    configMaster = JSON.parse(fs.readFileSync(configMasterFN,"utf8"))
 
-    if (fs.existsSync(configFN)){
-      try {
-        config = JSON.parse(fs.readFileSync(configFN,"utf8")) //json'd config file
-      } catch (e) {
+    if (fs.existsSync(configMasterFN)) {
+      configMaster = JSON.parse(fs.readFileSync(configMasterFN,"utf8"))
+    } 
+    if (fs.existsSync(configJsonFN)){
+      config = JSON.parse(fs.readFileSync(configJsonFN,"utf8"))
+    } else {
+      if (fs.existsSync(configJsFN)) {
+        config = require(configJsFN)
+        if (Array.isArray(config.greeting)){
+          config.general = {language:config.language,layout:config.layout,greeting:{midday:config.greeting,evening:config.greeting,morning:config.greeting,night:config.greeting}}
+        } else {
+          config.general = {language:config.language,layout:config.layout,greeting:config.greeting}
+        }
+        importHotwords(config.speech, config.speech.keyword, config.speech.model)
+        delete config.language
+        delete config.layout
+        delete config.greeting
+        delete config.speech.keyword
+        delete config.speech.model
+        fs.writeFileSync(configJsonFN,JSON.stringify(config,null,2),"utf8")
+        if (fs.existsSync(configJsonFN)){
+          fs.renameSync(configJsFN,configOldJsFN)
+        }
+      } else {
         config = configMaster
       }
-    } else {
-      config = configMaster
     }
 
+    
   }
-
+function importHotwords(obj, kwd, mdl){
+  obj.hotwords = []
+  if (Array.isArray(kwd) && Array.isArray(mdl)) {
+    if (kwd.length > mdl.length) {
+      var c = kwd.length
+    } else {
+      var c = mdl.length
+    }
+      for (var i = 0 ; i < c; i++ ){
+        if (!kwd[i]){
+          obj.hotwords.push({keyword:"unknown",model:mdl[i]})
+        } else if (mdl[i]) {
+          obj.hotwords.push({keyword:kwd[i],model:mdl[i]})
+        }
+      }
+  } else {
+    obj.hotwords.push({keyword:kwd,model:mdl})    
+  }
+}
 
 getFiles()
 // DO NOT REMOVE
